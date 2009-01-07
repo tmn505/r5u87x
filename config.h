bad4832734d20e1acb3355b9796b1c2b33dbb05b
Alex Hixon <alex@alexhixon.com>
2009-01-07 17:07:48 +1100

    Finish up renaming.

--- config.h
+++ config.h
@@ -3,8 +3,7 @@
 
 #define VERSION "0.2"
 
-#define PREFIX "/usr"
-#define UCODEDIR "/usr/lib/r5u87x/ucode/r5u87x-%vid%-%pid%.fw"
+//#define UCODE_PATH "/usr/lib/r5u87x/ucode/r5u87x-%vid%-%pid%.fw"
 
 // Provide support for --reload argument in application
 // so that we can reload the uvcvideo kernel module
